3d666c300759beeae3d59669fd518cce2af442fc
Jacques <jacques.tellier@hotmail.com>
2020-01-16 17:48:40 +0100

    Acte - Medecin - FonctionDocument

---
+++ Acte.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="Acte.css"/>
+
+    <title>Acte</title>
+  </head>
+
+  <body>
+
+  </body>
+</html>
---
+++ Medecin.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="Medecin.css"/>
+
+    <title>Medecin</title>
+  </head>
+
+  <body>
+
+  </body>
+</html>
